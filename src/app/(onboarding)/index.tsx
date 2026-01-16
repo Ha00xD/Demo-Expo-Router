8a96b79eefb60d4d4b11a7c1dcde648c8cf9dbab
KaungHtet Nyein <124788863+Ha00xD@users.noreply.github.com>
2026-01-16 13:34:52 +0630

    add onboarding

--- src/app/(onboarding)/index.tsx
+++ src/app/(onboarding)/index.tsx
@@ -1,27 +1,129 @@
-import { useStore } from "@/src/store/useStore";
+import React, { useState, useRef } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 import { useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { useStore } from "@/src/store/useStore";
+import { useTheme } from "@/src/hooks/useTheme";
+
+const { width } = Dimensions.get("window");
+
+const OnBoardingData = [
+  {
+    id: "1",
+    title: "Welcome",
+    desc: "Discover new features in 2026.",
+  },
+  {
+    id: "2",
+    title: "Connect",
+  },
+  {
+    id: "3",
+    title: "Launch",
+  },
+];
 
 const OnBoardingScreen = () => {
   const router = useRouter();
   const { setIsFirstTime } = useStore();
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const flatListRef = useRef<FlatList>(null);
 
-  const handleSkip = () => {
+  console.log("Check currentIndex>>>>", currentIndex);
+
+  const onViewableItemsChanged = useRef(({ viewableItems }: any) => {
+    console.log("CHECK VIEW ABLE ITEMS", viewableItems);
+    console.log("CHECK VIEW ABLE ITEMS INDEX", viewableItems[0].index);
+    if (viewableItems.length > 0) {
+      setCurrentIndex(viewableItems[0].index);
+    }
+  }).current;
+
+  const handleFinish = () => {
     setIsFirstTime(false);
     router.replace("/(auth)");
   };
 
-  return (
-    <View className="flex-1 bg-slate-500 justify-center items-center gap-3">
-      <Text>OnBoarding Screen</Text>
+  const nextSlide = () => {
+    if (currentIndex < OnBoardingData.length - 1) {
+      flatListRef.current?.scrollToIndex({ index: currentIndex + 1 });
+    } else {
+      handleFinish();
+    }
+  };
 
-      <TouchableOpacity
-        className="bg-slate-300 px-4 py-2 rounded-md"
-        onPress={handleSkip}
-        // onPress={() => router.push("/test")}
-      >
-        <Text>Skip</Text>
-      </TouchableOpacity>
+  const { colors } = useTheme();
+
+  return (
+    <View className="flex-1 bg-black">
+      <FlatList
+        ref={flatListRef}
+        onViewableItemsChanged={onViewableItemsChanged}
+        data={OnBoardingData}
+        keyExtractor={(item) => item.id}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{ width }}
+              className="flex-1 justify-center items-center"
+            >
+              <Text className="text-slate-100 text-2xl font-bold">
+                {item.title}
+              </Text>
+            </View>
+          );
+        }}
+      />
+      <View className="absolute bottom-10 px-5 left-0 right-0">
+        <View className="flex-row justify-between items-center">
+          <View className="flex-row gap-3">
+            {OnBoardingData.map((item, index) =>
+              index == currentIndex ? (
+                <View
+                  key={item.id}
+                  className="w-[10px] h-[10px] rounded-full bg-emerald-400"
+                />
+              ) : (
+                <View
+                  key={item.id}
+                  className="w-[10px] rounded-full h-[10px] bg-white"
+                />
+              )
+            )}
+          </View>
+          <View className="flex-row gap-5">
+            <TouchableOpacity
+              className="px-4 py-2 rounded-md"
+              style={{ backgroundColor: colors.text }}
+              onPress={handleFinish}
+            >
+              <Text className="text-sm" style={{ color: colors.primary }}>
+                Skip
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="px-4 py-2 rounded-md w-[100px]"
+              style={{ backgroundColor: colors.text }}
+              onPress={nextSlide}
+            >
+              <Text
+                className="text-sm"
+                style={{ color: colors.primary, alignSelf: "center" }}
+              >
+                {currentIndex == 2 ? "Get Started" : "Next"}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,45 +1,34 @@
 import "../../global.css";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { SQLiteProvider } from "expo-sqlite";
 import { StatusBar } from "expo-status-bar";
-import { useColorScheme } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import AppContext from "../constants/AppContext";
 import { initDb } from "../db/initDb";
 import { useTheme } from "../hooks/useTheme";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const insets = useSafeAreaInsets();
   const val = {};
-  const { colors } = useTheme();
-
-  console.log("Check colors>>>>>", colors);
+  const { isDark } = useTheme();
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <AppContext.Provider value={val}>
-        <SQLiteProvider
-          databaseName="app.db"
-          onInit={initDb}
-          options={{ useNewConnection: false }}
-        >
-          <Stack
-            initialRouteName="(auth)"
-            screenOptions={{
-              headerShown: false,
-              contentStyle: { paddingTop: insets.top },
-              animation: "none",
-            }}
-          />
-          <StatusBar style="auto" />
-        </SQLiteProvider>
-      </AppContext.Provider>
-    </ThemeProvider>
+    <AppContext.Provider value={val}>
+      <SQLiteProvider
+        databaseName="app.db"
+        onInit={initDb}
+        options={{ useNewConnection: false }}
+      >
+        <Stack
+          initialRouteName="(auth)"
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { paddingTop: insets.top },
+            animation: "none",
+          }}
+        />
+        <StatusBar style={isDark ? "dark" : "light"} animated={true} />
+      </SQLiteProvider>
+    </AppContext.Provider>
   );
 }
